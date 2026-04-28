5fd059d7bf7e70014a5d2f3c4c5637e94f6d8fc3
Bivash <dhakalbivash40@gmail.com>
2026-04-28 20:48:51 +0545

    Setup EF Core PostgreSQL database Connection

---
+++ VehiclePartsPro/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VehiclePartsPro.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets will be added later by your friend
+    }
+}
